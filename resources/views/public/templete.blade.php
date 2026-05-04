05aa673f8fca5cd2ca6292e16876f11b13d79bda
Israel Ndjondo <israelndjondo725@gmail.com>
2026-05-04 04:18:32 +0100

    style:integration de la mise en page et style du blog

---
+++ resources/views/public/templete.blade.php
@@ -0,0 +1,148 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+@vite(['resources/css/style.css'])
+
+<script src="./js/app.js" type="module" defer></script>
+<link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Barlow+Condensed:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Epunda+Sans:ital,wght@0,300..900;1,300..900&family=Merriweather:ital,opsz,wght@0,18..144,300..900;1,18..144,300..900&family=Open+Sans:ital,wght@0,300..800;1,300..800&family=Playfair+Display:ital,wght@0,400..900;1,400..900&family=Roboto:ital,wght@0,100..900;1,100..900&family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap" rel="stylesheet">
+
+    <title>Blog horty</title>
+</head>
+<body class="">
+
+
+    <header class="nav-navbar">
+        <div class="container">
+            <div class="nav-top">
+                <div class="nav-top-logo">
+                    Actus <span>Exode</span>
+                </div>
+              
+                <div class="nav-top-params">
+                    <div class="nav-top-menu">
+                        <a href="">home</a>
+                        <a href="">podcast</a>
+                        <a href="">music</a>
+                        <a href="">sport</a>
+                    </div>
+                    <div class="nav-top-langue-rech">
+                        <div class="">
+                            <svg class="search-icon">
+                                <use href="./svg/search.svg#search"></use>
+                            </svg>
+                        </div>
+                        <div class="">
+                            <div class="langues">fr</div>
+                            <div class=""></div>
+                        </div>    
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+
+     
+     <footer class="main-footer">
+        <div class="container">
+            <div class="">
+                <div class="">
+                    <h2 class="continant-title">continent</h2>
+                    <div class="par_continant">
+                       <div class="continant">
+                        <a href="./Admin/Actualite.html">afrique</a>
+                        <a href="./Admin/categorie.html">amerique</a>
+                        <a href="">europe</a>
+                       </div>
+                        <div class="asie_oceani">
+                            <a href="">asie</a>
+                            <a href="">oceanie</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="dark-mode form-check">
+                   
+                    <input type="checkbox" class="input-dark" id="mycheckbox" aria-labelledby="switch-label">
+                     <Label class="switch" role="switch" aria-checked="false"></Label>
+                    <span class="soleil">
+                        
+                    </span>
+                    <span class="nuit">
+                        
+                    </span>
+
+                </div>
+            </div>
+        </div>
+     </footer>
+      <div class="menu-footeur">
+            <div class="menus-nav container">
+            
+           <div class="menus-nav-icon actived">
+             <a href="">
+                <svg>
+                    <use href="./svg/sprite.svg#svg"></use>
+                </svg>
+             </a>
+                <div class="menus-nav-text">
+                    house
+                </div>
+           </div>
+           <div class="menus-nav-icon">
+             <a href="">
+                <svg>
+                    <use href="./svg/sprite.svg#podcasts"></use>
+                </svg>
+             </a>
+                <div class="menus-nav-text">
+                    podcast
+                </div>
+           </div>
+            <div class="menus-nav-icon">
+             <a href="">
+                <svg>
+                    <use href="./svg/sprite.svg#housed"></use>
+                </svg>
+             </a>
+                <div class="menus-nav-text">
+                    music
+                </div>
+           </div>
+           <div class="menus-nav-icon">
+             <a href="">
+                <svg>
+                    <use href="./svg/sprite.svg#sport"></use>
+                </svg>
+             </a>
+                <div class="menus-nav-text">
+                    sport
+                </div>
+           </div>
+           
+           <div class="menus-nav-icon">
+                <a href="">
+                    <svg>
+
+                        <use href="./svg/sprite.svg#menu"></use>
+                    </svg>
+                </a>
+                <div class="menus-nav-text">
+                    menu
+                </div>
+           </div>
+        </div>
+     </div>   
+
+
+     
+
+</body>
+</html>
+
